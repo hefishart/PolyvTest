6836d39b6cf2bda8dd089832e367997b58dbeeb6
hefishart <hefishart@hotmail.com>
2017-02-06 14:50:53 +0800

    添加项目文件。

---
+++ Polyv01/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polyv01.Startup))]
+namespace Polyv01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
